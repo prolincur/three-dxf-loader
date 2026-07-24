8e19c91135bca77bbef632c6c76ab11861200ba1
sonisourabh <59862118+sonisourabh@users.noreply.github.com>
2026-07-24 15:38:10 +0530

    fix async loading

--- src/loader/extend.js
+++ src/loader/extend.js
@@ -6,23 +6,23 @@ const THREEx = {}
 
 function load() {
   const key1 = 'FontLoader'
-  if (THREE[key1]) {
-    THREEx.FontLoader = THREE[key1]
-  } else {
-    import(/* webpackIgnore: true */ 'three/examples/jsm/loaders/FontLoader.js')
-      .then((module) => (THREEx.FontLoader = module.FontLoader))
-      .catch((error) => console.error(error))
-  }
+  const fontLoaderReady = THREE[key1]
+    ? Promise.resolve((THREEx.FontLoader = THREE[key1]))
+    : import(/* webpackIgnore: true */ 'three/examples/jsm/loaders/FontLoader.js')
+        .then((module) => (THREEx.FontLoader = module.FontLoader))
+        .catch((error) => console.error(error))
 
   const key2 = 'TextGeometry'
-  if (THREE[key2]) {
-    THREEx.TextGeometry = THREE[key2]
-  } else {
-    import(/* webpackIgnore: true */ 'three/examples/jsm/geometries/TextGeometry.js')
-      .then((module) => (THREEx.TextGeometry = module.TextGeometry))
-      .catch((error) => console.error(error))
-  }
+  const textGeometryReady = THREE[key2]
+    ? Promise.resolve((THREEx.TextGeometry = THREE[key2]))
+    : import(/* webpackIgnore: true */ 'three/examples/jsm/geometries/TextGeometry.js')
+        .then((module) => (THREEx.TextGeometry = module.TextGeometry))
+        .catch((error) => console.error(error))
+
+  return Promise.all([fontLoaderReady, textGeometryReady])
 }
-load()
+// Consumers that draw TEXT entities before this resolves can silently skip them
+// (see DXFLoader.loadEntities -> drawText) -- await THREEx.ready first if that matters.
+THREEx.ready = load()
 
 export default THREEx
